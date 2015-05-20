97cc87baf5f5f87872eeab8458d06e0ec50586c8
Ansgar_Lin <ansgar_lin@asus.com>
2015-05-20 17:54:36 +0800

    [CustomClassUtil][Update]: 1. Relocate DateUtil

---
+++ src/com/customclassutil/util/DateUtil.java
@@ -0,0 +1,59 @@
+package com.customclassutil.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+	/**
+	 * Get the date after days from today in format
+	 */
+	public static String getDateFromToday(int range, SimpleDateFormat format) {
+		Calendar today = Calendar.getInstance();
+		today.add(Calendar.DATE, range);
+
+		return format.format(today.getTime());
+	}
+
+	/**
+	 * Get today's date info by field
+	 */
+	public static String getToday(int field) {
+		Calendar today = Calendar.getInstance();
+		if (field == Calendar.MONTH) {
+			return String.valueOf(today.get(field) + 1);
+		} else {
+			return String.valueOf(today.get(field));
+		}
+	}
+
+	/**
+	 * Get calendar instance by format
+	 */
+	public static Calendar getCalenderByFormat(String dateString, String format) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+		Calendar calender = Calendar.getInstance();
+		calender.setTime(simpleDateFormat.parse(dateString));
+		return calender;
+	}
+
+	/**
+	 * Get date instance by format
+	 * 
+	 * @throws ParseException
+	 */
+	public static Date getDateByFormat(String dateString, String format) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+		return simpleDateFormat.parse(dateString);
+	}
+
+	/**
+	 * Get date duration, use date as basic unit
+	 * 
+	 * @throws ParseException
+	 */
+	public static int getDateDuration(Date startDate, Date endDate) {
+		return (int) Math.ceil((endDate.getTime() - startDate.getTime()) / (1000f * 3600f * 24f)); // A day in milliseconds
+	}
+}
--- src/com/customclassutil/util/MyUtils.java
+++ src/com/customclassutil/util/MyUtils.java
@@ -24,69 +24,12 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.BitSet;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 public class MyUtils {
 	public static String WHITE = "#ffffff";
-
-	public static class DateUtil {
-		/**
-		 * Get the date after days from today in format
-		 */
-		public static String getDateFromToday(int range, SimpleDateFormat format) {
-			Calendar today = Calendar.getInstance();
-			today.add(Calendar.DATE, range);
-
-			return format.format(today.getTime());
-		}
-
-		/**
-		 * Get today's date info by field
-		 */
-		public static String getToday(int field) {
-			Calendar today = Calendar.getInstance();
-			if (field == Calendar.MONTH) {
-				return String.valueOf(today.get(field) + 1);
-			} else {
-				return String.valueOf(today.get(field));
-			}
-		}
-
-		/**
-		 * Get calendar instance by format
-		 */
-		public static Calendar getCalenderByFormat(String dateString, String format) throws ParseException {
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-			Calendar calender = Calendar.getInstance();
-			calender.setTime(simpleDateFormat.parse(dateString));
-			return calender;
-		}
-
-		/**
-		 * Get date instance by format
-		 * 
-		 * @throws ParseException
-		 */
-		public static Date getDateByFormat(String dateString, String format) throws ParseException {
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-			return simpleDateFormat.parse(dateString);
-		}
-
-		/**
-		 * Get date duration, use date as basic unit
-		 * 
-		 * @throws ParseException
-		 */
-		public static int getDateDuration(Date startDate, Date endDate) {
-			return (int) Math.ceil((endDate.getTime() - startDate.getTime()) / (1000f * 3600f * 24f)); // A day in milliseconds
-		}
-	}
 
 	public static class ColorUtil {
 		/**
