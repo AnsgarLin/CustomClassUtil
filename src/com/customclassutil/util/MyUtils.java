40a6a0ef8d1948457b9607412c0dbb0a614eb296
Ansgar_Lin <ansgar_lin@asus.com>
2015-05-20 17:43:55 +0800

    [CustomClassUtil][Update]: 1. Relocate TypedValueUtil

--- src/com/customclassutil/util/MyUtils.java
+++ src/com/customclassutil/util/MyUtils.java
@@ -13,8 +13,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -37,29 +35,6 @@ import java.util.List;
 
 public class MyUtils {
 	public static String WHITE = "#ffffff";
-
-	public static class TypedValueUtil {
-		/**
-		 * Transfer value from given dimension unit to pixel, return -1 for unknown dimension
-		 */
-		public static float toPixel(DisplayMetrics dm, String unit, float value) {
-			if ((unit == "dip") || (unit == "dp")) {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, dm);
-			} else if (unit == "in") {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, value, dm);
-			} else if (unit == "mm") {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, value, dm);
-			} else if (unit == "pt") {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, value, dm);
-			} else if (unit == "px") {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, value, dm);
-			} else if (unit == "sp") {
-				return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, dm);
-			} else {
-				return -1;
-			}
-		}
-	}
 
 	public static class ToastUtil {
 		/**
