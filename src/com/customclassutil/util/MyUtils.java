78a67cad312059f8fa29d0b14096e838a762036d
Ansgar_Lin <ansgar_lin@asus.com>
2015-05-20 17:44:33 +0800

    [CustomClassUtil][Update]: 1. Relocate ToastUtil

--- src/com/customclassutil/util/MyUtils.java
+++ src/com/customclassutil/util/MyUtils.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -35,32 +34,6 @@ import java.util.List;
 
 public class MyUtils {
 	public static String WHITE = "#ffffff";
-
-	public static class ToastUtil {
-		/**
-		 * Re-trigger a toast by using the same toast instance
-		 */
-		public static Toast restart(Context context, Toast oldToast, String message) {
-			if (oldToast != null) {
-				oldToast.cancel();
-			}
-			oldToast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-			oldToast.show();
-			return oldToast;
-		}
-
-		/**
-		 * Re-trigger a toast by using the same toast instance
-		 */
-		public static Toast restart(Context context, Toast oldToast, int messageID) {
-			if (oldToast != null) {
-				oldToast.cancel();
-			}
-			oldToast = Toast.makeText(context, context.getString(messageID), Toast.LENGTH_SHORT);
-			oldToast.show();
-			return oldToast;
-		}
-	}
 
 	public static class DateUtil {
 		/**
