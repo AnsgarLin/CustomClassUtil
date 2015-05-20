27f9775fd90deab7d22a5396ccded7aa4b09aec9
Ansgar_Lin <ansgar_lin@asus.com>
2015-05-20 18:05:56 +0800

    [CustomClassUtil][Update]: 1. Relocate ColorUtil

--- src/com/customclassutil/util/MyUtils.java
+++ src/com/customclassutil/util/MyUtils.java
@@ -2,11 +2,9 @@ package com.customclassutil.util;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
@@ -29,58 +27,6 @@ import java.util.BitSet;
 import java.util.List;
 
 public class MyUtils {
-	public static String WHITE = "#ffffff";
-
-	public static class ColorUtil {
-		/**
-		 * Mix two color with the front in alpha
-		 */
-		public static String mixColor(String back, String cover, float coverAlpha) {
-			int[] backRGB = MyUtils.ColorUtil.hexToRGB(back);
-			int[] coverRGB = MyUtils.ColorUtil.hexToRGB(cover);
-			int[] mixRGB = new int[3];
-			for (int i = 0; i < backRGB.length; i++) {
-				mixRGB[i] = backRGB[i] + Math.round(((coverRGB[i] - backRGB[i]) * coverAlpha));
-			}
-			return MyUtils.ColorUtil.rgbToHex(mixRGB);
-		}
-
-		/**
-		 * Transform hex color string to integer RBG array
-		 */
-		public static int[] hexToRGB(String color) {
-			int[] numRGB = new int[3];
-			numRGB[0] = Integer.parseInt(color.substring(1, 3), 16);
-			numRGB[1] = Integer.parseInt(color.substring(3, 5), 16);
-			numRGB[2] = Integer.parseInt(color.substring(5, 7), 16);
-			return numRGB;
-		}
-
-		/**
-		 * Transform integer RBG to hex color string
-		 */
-		public static String rgbToHex(int[] color) {
-			String colorString = "#";
-			for (int element : color) {
-				if (element < 16) {
-					colorString += "0";
-				}
-				colorString += Integer.toHexString(element);
-			}
-			return colorString;
-		}
-
-		/**
-		 * Get color state list with press state. Press color will be mix with white in given alpha
-		 */
-		public static ColorStateList getColorStateList(String pressColor, String defaultColor, float mixAlpha) {
-			int[][] states = new int[][] { new int[] { android.R.attr.state_enabled, android.R.attr.state_pressed },
-					new int[] { android.R.attr.state_enabled } };
-			int[] colors = new int[] { Color.parseColor(mixColor(pressColor, WHITE, mixAlpha)), Color.parseColor(defaultColor) };
-			return new ColorStateList(states, colors);
-		}
-	}
-
 	public static class ListViewUtil {
 		/**
 		 * Calculate the scroll bar's top-left and bottom-right position on screen with limit
